891518f02ef9a3d87e9e524ab2aaabb008ae39b0
Troy <tydamon@hotmail.com>
2024-07-15 00:56:42 +1000

    added slingshot Matter.js demo to copy1 as template

--- client/src/components/Copy1.jsx
+++ client/src/components/Copy1.jsx
@@ -1,5 +1,17 @@
-import { useState, useEffect, useRef } from 'react';
-import Matter, { Engine, Render, World, Bodies, Body, Events, } from 'matter-js';
+import  { useState, useEffect, useRef } from 'react';
+import Matter, {
+  Engine,
+  Render,
+  World,
+  Bodies,
+  Body,
+  Events,
+  Composite,
+  Constraint,
+  Mouse,
+  MouseConstraint,
+} from 'matter-js';
+
 import PoolTable from './PoolTable';
 
 const Stripped = () => {
@@ -7,15 +19,13 @@ const Stripped = () => {
   const [balls, setBalls] = useState([]);
   const [ballSizes, setBallSizes] = useState([]);
   const [ballHits, setBallHits] = useState([]);
-  const [score, setScore] = useState(0);
- 
   const [cueBallPosition, setCueBallPosition] = useState({ x: 0, y: 0 });
-
   const gameRef = useRef();
 
   useEffect(() => {
     // Initialize Matter.js engine and rendering
     engine.world.gravity.y = 0;
+
     const render = Render.create({
       element: gameRef.current,
       engine,
@@ -23,6 +33,7 @@ const Stripped = () => {
         width: 1500,
         height: 680,
         wireframes: false,
+        //showAngleIndicator: true
       },
     });
     Render.run(render);
@@ -54,6 +65,51 @@ const Stripped = () => {
 
     Events.on(engine, 'beforeUpdate', updateCueBallPosition);
 
+    // SLINGSHOT //////////////////////////////////////////////////////////////
+ 
+    const rockOptions = { density: 0.004 };
+    let rock = Bodies.polygon(170, 450, 8, 20, rockOptions);
+    const anchor = { x: 170, y: 450 };
+    const elastic = Constraint.create({
+      pointA: anchor,
+      bodyB: rock,
+      length: 0.01,
+      damping: 0.01,
+      stiffness: 0.05,
+    });
+
+    Composite.add(engine.world, [ rock, elastic]);
+
+    // Add mouse control
+    const mouse = Mouse.create(render.canvas);
+    const mouseConstraint = MouseConstraint.create(engine, {
+      mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+      },
+    });
+
+    Composite.add(engine.world, mouseConstraint);
+    render.mouse = mouse;
+
+    // Update logic
+    Events.on(engine, 'afterUpdate', () => {
+      if (mouseConstraint.mouse.button === -1 && (rock.position.x > 190 || rock.position.y < 430)) {
+        // Limit maximum speed of the current rock.
+        if (Body.getSpeed(rock) > 45) {
+          Body.setSpeed(rock, 45);
+        }
+
+        // Release the current rock and add a new one.
+        rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
+        Composite.add(engine.world, rock);
+        elastic.bodyB = rock;
+      }
+    });
+
     return () => {
       Render.stop(render);
       World.clear(engine.world);
@@ -68,6 +124,7 @@ const Stripped = () => {
     const radius = ballRadii[radiusIndex];
     const startX = 1200 + Math.random() * 100;
     const startY = 300 + Math.random() * 100 - 50;
+
     const ball = Bodies.circle(startX, startY, radius, {
       frictionAir: 0,
       render: {
@@ -76,18 +133,17 @@ const Stripped = () => {
         lineWidth: 2,
       },
     });
+
     Body.setVelocity(ball, { x: 0, y: 0 });
-    setBalls(prev => [...prev, ball]);
-    setBallSizes(prev => [...prev, radius]);
-    setBallHits(prev => [...prev, 0]);
+    setBalls((prev) => [...prev, ball]);
+    setBallSizes((prev) => [...prev, radius]);
+    setBallHits((prev) => [...prev, 0]);
     World.add(engine.world, ball);
   };
 
   return (
     <div className="game-container" ref={gameRef}>
-
-      <PoolTable engine={engine} /> {/* Add the GreenTable component here */}
-
+      <PoolTable engine={engine} />
     </div>
   );
 };
